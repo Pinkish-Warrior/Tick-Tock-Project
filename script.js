787c72bc1830732c86f2a92b05a3421b26533120
Tania Rosa <78957963+Pinkish-Warrior@users.noreply.github.com>
2021-07-20 15:28:33 +0100

    fixing javaScript(stage2)

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const secondHand = document.querySelector('.seconds-hand');
-const minuteHand = document.querySelector('.minutes-hand');
-const hourHand = document.querySelector('.hours-hand');
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  
-  const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes /60) * 360) + 90;
-  minuteHand.style.transform = 'rotate(${minutesDegrees}deg)';
+  // secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  // const minutes = now.getMinutes();
+  // const minutesDegrees = ((minutes /60) * 360) + 90;
+  // minuteHand.style.transform = 'rotate(${minutesDegrees}deg)';
 
-  const hours = now.getHours();
-  const hoursDegrees = ((minutes / 12) * 360) + 90;
-  hourHand.style.transform = 'rotate(${hoursDegrees}deg)';
+  // const hours = now.getHours();
+  // const hoursDegrees = ((minutes / 12) * 360) + 90;
+  // hourHand.style.transform = 'rotate(${hoursDegrees}deg)';
+  console.log(seconds);
+
 }
 
 setInterval(setDate, 1000);
