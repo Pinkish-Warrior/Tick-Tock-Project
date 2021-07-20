9ec8b299358c9bf429ec56f715a48c97e905da19
Tania Rosa <78957963+Pinkish-Warrior@users.noreply.github.com>
2021-07-20 13:27:22 +0100

    fixing javascript formula

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = 'rotate(${secondsDegrees}deg)';
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes /60) * 360) + 90;
